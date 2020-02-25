7c9e70d41517e7e7bcd8ae1930a78968efc5a178
Ha Nguyen <hanguyen.0096@gmail.com>
2020-02-25 04:07:51 +0000

    fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ORIGIN: 'https://hanguyen-pokemongo-forum.now.sh/',
+  CLIENT_ORIGIN: 'https://hanguyen-pokemongo-forum.now.sh',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/pokemongo',
